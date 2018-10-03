84e7a5b699cbca2ada0af754b0c0daafce0a0d5e
IS Choi <insil321@hotmail.com>
2018-10-03 05:31:37 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.using_pop
+end
+
+def pop_with_args(array)
+  array.pop
+end
+
+def using_shift(array)
+  array.using_shift
+end
+
+def shift_with_args(array)
+  array.shift
+end
+
+def using_concat(array1, array2)
+  
